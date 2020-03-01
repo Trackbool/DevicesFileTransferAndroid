256af5b42b14539f7755fce4d864955ad1f3d8b0
Adrián Fernández Arnal <mistert430@gmail.com>
2020-03-01 20:26:39 +0100

    Show Android version in os information

--- app/src/main/java/com/afa/devicesfiletransfer/util/SystemUtils.java
+++ app/src/main/java/com/afa/devicesfiletransfer/util/SystemUtils.java
@@ -1,5 +1,6 @@
 package com.afa.devicesfiletransfer.util;
 
+import android.os.Build;
 import android.os.Environment;
 
 import java.io.File;
@@ -7,13 +8,13 @@ import java.io.File;
 public class SystemUtils {
     public static String getSystemName() {
         String name = android.os.Build.MODEL;
-        if(name != null && name.length() > 0) return name;
+        if (name != null && name.length() > 0) return name;
 
         return "Unknown";
     }
 
     public static String getOs() {
-        return "Android";
+        return "Android " + Build.VERSION.RELEASE;
     }
 
     public static File getDownloadsDirectory() {
